812e54d7e863ff2b13929d6038a95b9acddfe5f4
Sergey Kuchin <sergey.kuchin@fastdev.se>
2018-08-30 18:00:03 +0500

    it is moving but can't move in two directions in same time

--- app/src/main/java/com/example/sergeykuchin/argame/MovingNode.kt
+++ app/src/main/java/com/example/sergeykuchin/argame/MovingNode.kt
@@ -10,8 +10,18 @@ import android.view.animation.LinearInterpolator
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.math.Vector3Evaluator
+import java.util.*
 
 abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+
+    private enum class Directions {
+        X_RIGHT,
+        X_LEFT,
+        Y_UP,
+        Y_DOWN,
+        Z_FORWARD,
+        Z_BACKWARD
+    }
 
     private var xRight = false
     private var xLeft = false
@@ -19,6 +29,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     private var yDown = false
     private var zForward = false
     private var zBackward = false
+
+    private val releasesQueue = PriorityQueue<Directions>()
 
     /**
      * Multiplier to each movement
@@ -31,7 +43,6 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     @Volatile
     protected var objectAnimator: ObjectAnimator? = null
 
-    @Volatile
     protected var movementStatus: MovementStatus = MovementStatus.STOPPED
 
     private var movementThread: Thread? = null
@@ -45,7 +56,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseUp() {
-        yUp = false
+        //yUp = false
+        releasesQueue.add(Directions.Y_UP)
     }
 
     override fun goDown() {
@@ -55,7 +67,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseDown() {
-        yDown = false
+        //yDown = false
+        releasesQueue.add(Directions.Y_DOWN)
     }
 
 
@@ -66,7 +79,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseRight() {
-        xRight = false
+        //xRight = false
+        releasesQueue.add(Directions.X_RIGHT)
     }
 
     override fun goLeft() {
@@ -76,7 +90,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseLeft() {
-        xLeft = false
+        //xLeft = false
+        releasesQueue.add(Directions.X_LEFT)
     }
 
 
@@ -88,7 +103,8 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseForward() {
-        zForward = false
+        //zForward = false
+        releasesQueue.add(Directions.Z_FORWARD)
     }
 
     override fun goBackward() {
@@ -98,7 +114,22 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     override fun releaseBackward() {
-        zBackward = false
+        //zBackward = false
+        releasesQueue.add(Directions.Z_BACKWARD)
+    }
+
+    private fun releaseButtons() {
+        releasesQueue.forEach { direction ->
+            when (direction) {
+                Directions.Y_UP -> yUp = false
+                Directions.Y_DOWN -> yDown = false
+                Directions.X_RIGHT -> xRight = false
+                Directions.X_LEFT -> xLeft = false
+                Directions.Z_FORWARD -> zForward = false
+                Directions.Z_BACKWARD -> zBackward = false
+                else -> {}
+            }
+        }
     }
 
     fun startMovementHandler() {
@@ -135,23 +166,23 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     private fun setNewPosition() {
-        if (xRight) {
+        if (xRight || releasesQueue.contains(Directions.X_RIGHT)) {
             objectAnimator?.setObjectValues(worldPosition, updateVector3(newX = (worldPosition.x + movementStep), oldVector3 = worldPosition))
         }
-        if (xLeft) {
+        if (xLeft || releasesQueue.contains(Directions.X_LEFT)) {
             objectAnimator?.setObjectValues(worldPosition, updateVector3(newX = (worldPosition.x - movementStep), oldVector3 = worldPosition))
         }
-        if (yUp) {
+        if (yUp || releasesQueue.contains(Directions.Y_UP)) {
             objectAnimator?.setObjectValues(worldPosition, updateVector3(newY = (worldPosition.y + movementStep), oldVector3 = worldPosition))
         }
-        if (yDown) {
+        if (yDown || releasesQueue.contains(Directions.Y_DOWN)) {
             objectAnimator?.setObjectValues(worldPosition, updateVector3(newY = (worldPosition.y - movementStep), oldVector3 = worldPosition))
         }
-        if (zForward) {
-            objectAnimator?.setObjectValues(worldPosition, updateVector3(newZ = (worldPosition.z + movementStep), oldVector3 = worldPosition))
-        }
-        if (zBackward) {
+        if (zForward || releasesQueue.contains(Directions.Z_FORWARD)) {
             objectAnimator?.setObjectValues(worldPosition, updateVector3(newZ = (worldPosition.z - movementStep), oldVector3 = worldPosition))
+        }
+        if (zBackward || releasesQueue.contains(Directions.Z_BACKWARD)) {
+            objectAnimator?.setObjectValues(worldPosition, updateVector3(newZ = (worldPosition.z + movementStep), oldVector3 = worldPosition))
         }
     }
 
@@ -169,11 +200,17 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
     }
 
     protected fun updateVector3(newX: Float? = null, newY: Float? = null, newZ: Float? = null, oldVector3: Vector3): Vector3 {
-        newX?.let { oldVector3.x = newX }
-        newY?.let { oldVector3.y = newY }
-        newZ?.let { oldVector3.z = newZ }
+        val newVector3 = Vector3()
 
-        return oldVector3
+        newVector3.x = oldVector3.x
+        newVector3.y = oldVector3.y
+        newVector3.z = oldVector3.z
+
+        newX?.let { newVector3.x = newX }
+        newY?.let { newVector3.y = newY }
+        newZ?.let { newVector3.z = newZ }
+
+        return newVector3
     }
 
     private fun isEveryButtonReleased(): Boolean = !xRight && !xLeft && !yUp && !yDown && !zForward && !zBackward
@@ -187,27 +224,37 @@ abstract class MovingNode: Node(), Controls, ValueAnimator.AnimatorUpdateListene
 
     }
 
-    override fun onAnimationEnd(p0: Animator?) {
+    override fun onAnimationEnd(animator: Animator?) {
+
+        val endValue = (animator as ObjectAnimator).animatedValue as Vector3
+        worldPosition = endValue
+        setNewPosition()
         when (movementStatus) {
             MovementStatus.STARTING -> {
-                setNewPosition()
-                movementStatus = MovementStatus.MOVING
-                objectAnimator?.interpolator = LinearInterpolator()
+                if (isEveryButtonReleased()) {
+                    movementStatus = MovementStatus.ENDING
+                    objectAnimator?.interpolator = DecelerateInterpolator()
+                } else {
+                    movementStatus = MovementStatus.MOVING
+                    objectAnimator?.interpolator = LinearInterpolator()
+
+                }
                 objectAnimator?.start()
             }
             MovementStatus.MOVING -> {
                 if (isEveryButtonReleased()) {
-                    setNewPosition()
                     movementStatus = MovementStatus.ENDING
                     objectAnimator?.interpolator = DecelerateInterpolator()
-                    objectAnimator?.start()
                 }
+                objectAnimator?.start()
             }
             MovementStatus.ENDING -> {
                 movementStatus = MovementStatus.STOPPED
+                releasesQueue.clear()
             }
             else -> {}
         }
+        releaseButtons()
     }
 
     override fun onAnimationCancel(p0: Animator?) {
